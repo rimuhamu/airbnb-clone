91ed5cf2fc4675c09e6c9079136196e9c988fba9
Emir Risyad <emirrsyd@gmail.com>
2023-07-03 23:15:33 +0200

    add empty page result

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return <EmptyState showReset />;
+  }
   return (
-    <div>
-      HElno
-    </div>
-  )
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        <div>My furture listing</div>
+      </div>
+    </Container>
+  );
 }
