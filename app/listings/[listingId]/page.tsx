d8ed848bae32bdf7813f55d52452bb819ffb23f6
Emir Risyad <emirrsyd@gmail.com>
2023-11-22 23:51:12 +0100

    individual listing page

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,11 +1,31 @@
+import getCurrentUser from '@/actions/getCurrentUser';
 import getListingById from '@/actions/getListingById';
+import ClientOnly from '@/components/ClientOnly';
+import EmptyState from '@/components/EmptyState';
+import { ListingClient } from './ListingClient';
 
 interface Params {
   listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: Params }) => {
-  const listing = await getListingById;
-  return <div>ListingPage</div>;
+  const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
 };
 export default ListingPage;
