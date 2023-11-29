a520a9e52301cfd6e1dfb09331a8ccefe2ed36ee
Emir Risyad <emirrsyd@gmail.com>
2023-11-29 16:12:58 +0100

    properties screen

--- actions/getListings.ts
+++ actions/getListings.ts
@@ -1,8 +1,21 @@
 import prisma from '@/libs/prismadb';
 
-export default async function getListings() {
+export interface ListingsParams {
+  userId?: string;
+}
+
+export default async function getListings(params: ListingsParams) {
   try {
+    const { userId } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: 'desc',
       },
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import getCurrentUser from '@/actions/getCurrentUser';
-import getListings from '@/actions/getListings';
+import getListings, { ListingsParams } from '@/actions/getListings';
 import Container from '@/components/Container';
 import EmptyState from '@/components/EmptyState';
 import ListingCard from '@/components/listings/ListingCard';
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: ListingsParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
@@ -26,4 +30,6 @@ export default async function Home() {
       </div>
     </Container>
   );
-}
+};
+
+export default Home;
